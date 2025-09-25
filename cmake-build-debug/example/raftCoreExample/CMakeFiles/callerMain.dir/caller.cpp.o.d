d942b6be334d29a4391161414582cfd0a9cb2226
hansao <a2472777914@163.com>
2025-09-25 22:20:21 +0800

    -

--- cmake-build-debug/example/raftCoreExample/CMakeFiles/callerMain.dir/caller.cpp.o.d
+++ cmake-build-debug/example/raftCoreExample/CMakeFiles/callerMain.dir/caller.cpp.o.d
@@ -155,32 +155,44 @@ example/raftCoreExample/CMakeFiles/callerMain.dir/caller.cpp.o: \
  /usr/include/c++/11/bits/locale_facets.tcc \
  /usr/include/c++/11/bits/basic_ios.tcc \
  /usr/include/c++/11/bits/ostream.tcc /usr/include/c++/11/istream \
- /usr/include/c++/11/bits/istream.tcc \
- /home/hanhan/Desktop/4/src/raftClerk/include/clerk.h \
- /usr/include/arpa/inet.h /usr/include/netinet/in.h \
- /usr/include/x86_64-linux-gnu/sys/socket.h \
- /usr/include/x86_64-linux-gnu/bits/types/struct_iovec.h \
- /usr/include/x86_64-linux-gnu/bits/socket.h \
- /usr/include/x86_64-linux-gnu/bits/socket_type.h \
- /usr/include/x86_64-linux-gnu/bits/sockaddr.h \
- /usr/include/x86_64-linux-gnu/asm/socket.h \
- /usr/include/asm-generic/socket.h /usr/include/linux/posix_types.h \
- /usr/include/linux/stddef.h \
- /usr/include/x86_64-linux-gnu/asm/posix_types.h \
- /usr/include/x86_64-linux-gnu/asm/posix_types_64.h \
- /usr/include/asm-generic/posix_types.h \
- /usr/include/x86_64-linux-gnu/asm/bitsperlong.h \
- /usr/include/asm-generic/bitsperlong.h \
- /usr/include/x86_64-linux-gnu/asm/sockios.h \
- /usr/include/asm-generic/sockios.h \
- /usr/include/x86_64-linux-gnu/bits/types/struct_osockaddr.h \
- /usr/include/x86_64-linux-gnu/bits/in.h \
- /home/hanhan/Desktop/4/src/raftClerk/include/raftServerRpcUtil.h \
- /home/hanhan/Desktop/4/src/raftRpcPro/include/kvServerRPC.pb.h \
- /usr/include/c++/11/limits /usr/include/google/protobuf/port_def.inc \
- /usr/include/google/protobuf/port_undef.inc \
- /usr/include/google/protobuf/io/coded_stream.h /usr/include/assert.h \
- /usr/include/c++/11/atomic /usr/include/c++/11/bits/atomic_base.h \
+ /usr/include/c++/11/bits/istream.tcc /usr/include/c++/11/vector \
+ /usr/include/c++/11/bits/stl_uninitialized.h \
+ /usr/include/c++/11/bits/stl_vector.h \
+ /usr/include/c++/11/bits/stl_bvector.h \
+ /usr/include/c++/11/bits/vector.tcc /usr/include/c++/11/random \
+ /usr/include/c++/11/cmath /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h \
+ /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
+ /usr/include/c++/11/bits/specfun.h /usr/include/c++/11/limits \
+ /usr/include/c++/11/tr1/gamma.tcc \
+ /usr/include/c++/11/tr1/special_function_util.h \
+ /usr/include/c++/11/tr1/bessel_function.tcc \
+ /usr/include/c++/11/tr1/beta_function.tcc \
+ /usr/include/c++/11/tr1/ell_integral.tcc \
+ /usr/include/c++/11/tr1/exp_integral.tcc \
+ /usr/include/c++/11/tr1/hypergeometric.tcc \
+ /usr/include/c++/11/tr1/legendre_function.tcc \
+ /usr/include/c++/11/tr1/modified_bessel_func.tcc \
+ /usr/include/c++/11/tr1/poly_hermite.tcc \
+ /usr/include/c++/11/tr1/poly_laguerre.tcc \
+ /usr/include/c++/11/tr1/riemann_zeta.tcc \
+ /usr/include/c++/11/bits/random.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/opt_random.h \
+ /usr/include/c++/11/bits/random.tcc /usr/include/c++/11/numeric \
+ /usr/include/c++/11/bits/stl_numeric.h /usr/include/c++/11/bit \
+ /usr/include/c++/11/pstl/glue_numeric_defs.h \
+ /usr/include/c++/11/pstl/execution_defs.h /usr/include/c++/11/chrono \
+ /usr/include/c++/11/ratio /usr/include/c++/11/ctime \
+ /usr/include/c++/11/bits/parse_numbers.h /usr/include/c++/11/thread \
+ /usr/include/c++/11/stop_token /usr/include/c++/11/atomic \
+ /usr/include/c++/11/bits/atomic_base.h \
  /usr/include/c++/11/bits/atomic_lockfree_defines.h \
  /usr/include/c++/11/bits/atomic_wait.h /usr/include/c++/11/climits \
  /usr/lib/gcc/x86_64-linux-gnu/11/include/limits.h \
@@ -202,9 +214,51 @@ example/raftCoreExample/CMakeFiles/callerMain.dir/caller.cpp.o: \
  /usr/include/x86_64-linux-gnu/asm/unistd.h \
  /usr/include/x86_64-linux-gnu/asm/unistd_64.h \
  /usr/include/x86_64-linux-gnu/bits/syscall.h \
- /usr/include/c++/11/bits/std_mutex.h /usr/include/c++/11/cstddef \
- /usr/include/c++/11/cstring /usr/include/string.h /usr/include/strings.h \
+ /usr/include/c++/11/bits/std_mutex.h \
+ /usr/include/c++/11/bits/std_thread.h /usr/include/c++/11/tuple \
  /usr/include/c++/11/utility /usr/include/c++/11/bits/stl_relops.h \
+ /usr/include/c++/11/array /usr/include/c++/11/bits/uses_allocator.h \
+ /usr/include/c++/11/bits/invoke.h /usr/include/c++/11/bits/refwrap.h \
+ /usr/include/c++/11/bits/unique_ptr.h /usr/include/c++/11/semaphore \
+ /usr/include/c++/11/bits/semaphore_base.h \
+ /usr/include/c++/11/bits/atomic_timed_wait.h \
+ /usr/include/c++/11/bits/this_thread_sleep.h \
+ /usr/include/x86_64-linux-gnu/sys/time.h /usr/include/semaphore.h \
+ /usr/include/x86_64-linux-gnu/bits/semaphore.h \
+ /usr/include/c++/11/unordered_map \
+ /usr/include/c++/11/ext/aligned_buffer.h \
+ /usr/include/c++/11/bits/hashtable.h \
+ /usr/include/c++/11/bits/hashtable_policy.h \
+ /usr/include/c++/11/bits/enable_special_members.h \
+ /usr/include/c++/11/bits/node_handle.h \
+ /usr/include/c++/11/bits/unordered_map.h \
+ /usr/include/c++/11/bits/erase_if.h \
+ /home/hanhan/Desktop/4/src/raftClerk/include/clerk.h \
+ /usr/include/arpa/inet.h /usr/include/netinet/in.h \
+ /usr/include/x86_64-linux-gnu/sys/socket.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_iovec.h \
+ /usr/include/x86_64-linux-gnu/bits/socket.h \
+ /usr/include/x86_64-linux-gnu/bits/socket_type.h \
+ /usr/include/x86_64-linux-gnu/bits/sockaddr.h \
+ /usr/include/x86_64-linux-gnu/asm/socket.h \
+ /usr/include/asm-generic/socket.h /usr/include/linux/posix_types.h \
+ /usr/include/linux/stddef.h \
+ /usr/include/x86_64-linux-gnu/asm/posix_types.h \
+ /usr/include/x86_64-linux-gnu/asm/posix_types_64.h \
+ /usr/include/asm-generic/posix_types.h \
+ /usr/include/x86_64-linux-gnu/asm/bitsperlong.h \
+ /usr/include/asm-generic/bitsperlong.h \
+ /usr/include/x86_64-linux-gnu/asm/sockios.h \
+ /usr/include/asm-generic/sockios.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_osockaddr.h \
+ /usr/include/x86_64-linux-gnu/bits/in.h \
+ /home/hanhan/Desktop/4/src/raftClerk/include/raftServerRpcUtil.h \
+ /home/hanhan/Desktop/4/src/raftRpcPro/include/kvServerRPC.pb.h \
+ /usr/include/google/protobuf/port_def.inc \
+ /usr/include/google/protobuf/port_undef.inc \
+ /usr/include/google/protobuf/io/coded_stream.h /usr/include/assert.h \
+ /usr/include/c++/11/cstddef /usr/include/c++/11/cstring \
+ /usr/include/string.h /usr/include/strings.h \
  /usr/include/x86_64-linux-gnu/sys/param.h /usr/include/signal.h \
  /usr/include/x86_64-linux-gnu/bits/signum-generic.h \
  /usr/include/x86_64-linux-gnu/bits/signum-arch.h \
@@ -234,31 +288,14 @@ example/raftCoreExample/CMakeFiles/callerMain.dir/caller.cpp.o: \
  /usr/include/c++/11/algorithm /usr/include/c++/11/bits/ranges_algo.h \
  /usr/include/c++/11/bits/ranges_algobase.h /usr/include/c++/11/iterator \
  /usr/include/c++/11/bits/stream_iterator.h \
- /usr/include/c++/11/bits/invoke.h /usr/include/c++/11/bits/ranges_util.h \
+ /usr/include/c++/11/bits/ranges_util.h \
  /usr/include/c++/11/pstl/glue_algorithm_defs.h \
- /usr/include/c++/11/functional /usr/include/c++/11/tuple \
- /usr/include/c++/11/array /usr/include/c++/11/bits/uses_allocator.h \
- /usr/include/c++/11/bits/refwrap.h \
- /usr/include/c++/11/bits/std_function.h \
- /usr/include/c++/11/unordered_map \
- /usr/include/c++/11/ext/aligned_buffer.h \
- /usr/include/c++/11/bits/hashtable.h \
- /usr/include/c++/11/bits/hashtable_policy.h \
- /usr/include/c++/11/bits/enable_special_members.h \
- /usr/include/c++/11/bits/node_handle.h \
- /usr/include/c++/11/bits/unordered_map.h \
- /usr/include/c++/11/bits/erase_if.h /usr/include/c++/11/vector \
- /usr/include/c++/11/bits/stl_uninitialized.h \
- /usr/include/c++/11/bits/stl_vector.h \
- /usr/include/c++/11/bits/stl_bvector.h \
- /usr/include/c++/11/bits/vector.tcc \
- /usr/include/c++/11/pstl/execution_defs.h /usr/include/c++/11/map \
- /usr/include/c++/11/bits/stl_tree.h /usr/include/c++/11/bits/stl_map.h \
+ /usr/include/c++/11/functional /usr/include/c++/11/bits/std_function.h \
+ /usr/include/c++/11/map /usr/include/c++/11/bits/stl_tree.h \
+ /usr/include/c++/11/bits/stl_map.h \
  /usr/include/c++/11/bits/stl_multimap.h /usr/include/c++/11/memory \
  /usr/include/c++/11/bits/stl_raw_storage_iter.h \
- /usr/include/c++/11/bits/align.h /usr/include/c++/11/bit \
- /usr/include/c++/11/bits/unique_ptr.h \
- /usr/include/c++/11/bits/shared_ptr.h \
+ /usr/include/c++/11/bits/align.h /usr/include/c++/11/bits/shared_ptr.h \
  /usr/include/c++/11/bits/shared_ptr_base.h \
  /usr/include/c++/11/bits/allocated_ptr.h \
  /usr/include/c++/11/ext/concurrence.h \
@@ -288,8 +325,6 @@ example/raftCoreExample/CMakeFiles/callerMain.dir/caller.cpp.o: \
  /usr/include/google/protobuf/message_lite.h \
  /usr/include/google/protobuf/metadata_lite.h \
  /usr/include/google/protobuf/stubs/once.h /usr/include/c++/11/mutex \
- /usr/include/c++/11/chrono /usr/include/c++/11/ratio \
- /usr/include/c++/11/ctime /usr/include/c++/11/bits/parse_numbers.h \
  /usr/include/c++/11/bits/unique_lock.h \
  /usr/include/google/protobuf/map_type_handler.h \
  /usr/include/google/protobuf/parse_context.h \
@@ -316,33 +351,6 @@ example/raftCoreExample/CMakeFiles/callerMain.dir/caller.cpp.o: \
  /usr/include/google/protobuf/extension_set.h /usr/include/c++/11/cassert \
  /usr/include/google/protobuf/service.h \
  /home/hanhan/Desktop/4/src/rpc/include/mprpcchannel.h \
- /usr/include/c++/11/random /usr/include/c++/11/cmath /usr/include/math.h \
- /usr/include/x86_64-linux-gnu/bits/math-vector.h \
- /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
- /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
- /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
- /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
- /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
- /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
- /usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h \
- /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
- /usr/include/c++/11/bits/specfun.h /usr/include/c++/11/tr1/gamma.tcc \
- /usr/include/c++/11/tr1/special_function_util.h \
- /usr/include/c++/11/tr1/bessel_function.tcc \
- /usr/include/c++/11/tr1/beta_function.tcc \
- /usr/include/c++/11/tr1/ell_integral.tcc \
- /usr/include/c++/11/tr1/exp_integral.tcc \
- /usr/include/c++/11/tr1/hypergeometric.tcc \
- /usr/include/c++/11/tr1/legendre_function.tcc \
- /usr/include/c++/11/tr1/modified_bessel_func.tcc \
- /usr/include/c++/11/tr1/poly_hermite.tcc \
- /usr/include/c++/11/tr1/poly_laguerre.tcc \
- /usr/include/c++/11/tr1/riemann_zeta.tcc \
- /usr/include/c++/11/bits/random.h \
- /usr/include/x86_64-linux-gnu/c++/11/bits/opt_random.h \
- /usr/include/c++/11/bits/random.tcc /usr/include/c++/11/numeric \
- /usr/include/c++/11/bits/stl_numeric.h \
- /usr/include/c++/11/pstl/glue_numeric_defs.h \
  /home/hanhan/Desktop/4/src/rpc/include/mprpccontroller.h \
  /home/hanhan/Desktop/4/src/rpc/include/rpcprovider.h \
  /usr/local/include/muduo/net/EventLoop.h /usr/include/boost/any.hpp \
@@ -750,14 +758,7 @@ example/raftCoreExample/CMakeFiles/callerMain.dir/caller.cpp.o: \
  /usr/include/boost/archive/detail/basic_oserializer.hpp \
  /usr/include/boost/archive/detail/basic_pointer_oserializer.hpp \
  /usr/include/boost/serialization/string.hpp \
- /usr/include/c++/11/condition_variable /usr/include/c++/11/stop_token \
- /usr/include/c++/11/bits/std_thread.h /usr/include/c++/11/semaphore \
- /usr/include/c++/11/bits/semaphore_base.h \
- /usr/include/c++/11/bits/atomic_timed_wait.h \
- /usr/include/c++/11/bits/this_thread_sleep.h \
- /usr/include/x86_64-linux-gnu/sys/time.h /usr/include/semaphore.h \
- /usr/include/x86_64-linux-gnu/bits/semaphore.h /usr/include/c++/11/queue \
+ /usr/include/c++/11/condition_variable /usr/include/c++/11/queue \
  /usr/include/c++/11/deque /usr/include/c++/11/bits/stl_deque.h \
  /usr/include/c++/11/bits/deque.tcc /usr/include/c++/11/bits/stl_queue.h \
- /usr/include/c++/11/thread \
  /home/hanhan/Desktop/4/src/common/include/config.h
--- example/raftCoreExample/caller.cpp
+++ example/raftCoreExample/caller.cpp
@@ -7,10 +7,9 @@
 #include <unordered_map>
 #include "clerk.h"
 #include "util.h"
-#include "monsoon.h"  // 协程调度器
-#include "mprpccontroller.h"  // RPC错误处理
+#include "mprpccontroller.h"  // RPC错误处理（非协程，保留）
 
-// 业务常量定义
+// 业务常量定义（不变）
 const int kTotalOps = 10000;          // 总操作次数
 const int kHotKeyRatio = 20;          // 热点键占比（20%）
 const int kMaxRetries = 3;            // 最大重试次数
@@ -20,12 +19,12 @@ const std::vector<std::string> kKeyPrefixes = {
     "order:"   // 订单信息键
 };
 
-// 生成随机业务键（含热点键逻辑）
+// 生成随机业务键（含热点键逻辑，不变）
 std::string generateKey(std::mt19937& rng) {
     std::uniform_int_distribution<int> prefixDist(0, kKeyPrefixes.size() - 1);
     std::string prefix = kKeyPrefixes[prefixDist(rng)];
 
-    // 20%概率生成热点键（固定后缀），80%生成随机键
+    // 20%概率生成热点键，80%生成随机键
     std::uniform_int_distribution<int> hotDist(0, 99);
     if (hotDist(rng) < kHotKeyRatio) {
         return prefix + "hot";  // 热点键：如user:hot、goods:hot
@@ -35,7 +34,7 @@ std::string generateKey(std::mt19937& rng) {
     }
 }
 
-// 生成随机值（修复时间戳转换问题）
+// 生成随机值（修复时间戳转换问题，不变）
 std::string generateValue(std::mt19937& rng) {
     std::uniform_int_distribution<int> valDist(100000, 999999);
     // 将 time_point 转换为毫秒级时间戳
@@ -45,10 +44,10 @@ std::string generateValue(std::mt19937& rng) {
     return "{\"data\":" + std::to_string(valDist(rng)) + ",\"ts\":" + std::to_string(now_ms) + "}";
 }
 
-// 业务操作类型
-enum OpType { PUT, GET, DELETE };
+// 业务操作类型（不变，暂不处理DELETE以避免未定义行为）
+enum OpType { PUT, GET };
 
-// 执行单次业务操作（带重试逻辑）
+// 执行单次业务操作（带重试逻辑，不变）
 bool executeOp(Clerk& client, OpType op, const std::string& key, const std::string& value, int opId) {
     MprpcController controller;  // 用于RPC错误检测
     for (int retry = 0; retry < kMaxRetries; ++retry) {
@@ -65,15 +64,15 @@ bool executeOp(Clerk& client, OpType op, const std::string& key, const std::stri
                     std::cout << "[Op" << opId << "] GET " << key << " -> " << (res.empty() ? "null" : res) << "\n";
                     break;
                 }
-                
             }
             // 无错误则返回成功
             return true;
         } catch (...) {
-            // 捕获RPC或业务异常（需Clerk抛出异常或通过controller反馈）
+            // 捕获RPC或业务异常
             if (controller.Failed()) {
                 std::cerr << "[Op" << opId << "] Retry " << retry + 1 << " failed: " << controller.ErrorText() << "\n";
-                std::this_thread::sleep_for(std::chrono::milliseconds(100 * (retry + 1)));  // 指数退避重试
+                // 指数退避重试（普通线程睡眠，非协程睡眠）
+                std::this_thread::sleep_for(std::chrono::milliseconds(100 * (retry + 1)));
             } else {
                 std::cerr << "[Op" << opId << "] Unknown error, retry " << retry + 1 << "\n";
             }
@@ -83,12 +82,12 @@ bool executeOp(Clerk& client, OpType op, const std::string& key, const std::stri
     return false;
 }
 
-// 协程任务：模拟单个"用户"的一系列操作
+// 普通任务函数：模拟单个"用户"的一系列操作（移除协程依赖，不变）
 void userTask(Clerk& client, int userId, int startOpId, int opCount) {
-    // 每个用户独立随机数生成器（避免并发冲突）
+    // 每个任务独立随机数生成器（避免并发冲突）
     std::random_device rd;
     std::mt19937 rng(rd() + userId);  // 用userId偏移，保证随机性
-    std::uniform_int_distribution<int> opDist(0, 2);  // 0:PUT, 1:GET, 2:DELETE
+    std::uniform_int_distribution<int> opDist(0, 1);  // 仅PUT/GET，避免DELETE未实现问题
 
     for (int i = 0; i < opCount; ++i) {
         int opId = startOpId + i;
@@ -99,45 +98,48 @@ void userTask(Clerk& client, int userId, int startOpId, int opCount) {
         // 执行操作并记录成功率
         executeOp(client, op, key, value, opId);
 
-        // 模拟用户操作间隔（随机10-100ms）
+        // 模拟用户操作间隔（普通线程睡眠，非协程睡眠）
         std::uniform_int_distribution<int> delayDist(10, 100);
         std::this_thread::sleep_for(std::chrono::milliseconds(delayDist(rng)));
     }
 }
 
 int main(int argc, char**argv) {
-    // 初始化客户端（从配置文件读取集群信息）
+    // 1. 初始化客户端（检查返回值，不变）
     Clerk client;
     client.Init("test.conf");
-       
 
-    // 配置并发参数
-    int threadNum = 8;  // 模拟8个并发用户线程
+    // 2. 配置执行参数（移除协程调度器，改用普通线程）
+    int threadNum = 1;  // 可选：1=单线程（简单稳定），8=普通多线程（无协程）
     int opsPerThread = kTotalOps / threadNum;
+    std::vector<std::thread> threads;  // 普通线程容器（替代协程调度器）
 
-    // 启动协程调度器（利用项目中的fiber框架提高并发效率）
-    monsoon::IOManager iom(threadNum);  // 线程数=并发用户数
+    // 3. 记录开始时间（不变）
     auto start = now();
 
-    // 分配任务到协程
+    // 4. 分配任务到普通线程（替代协程scheduler）
     for (int i = 0; i < threadNum; ++i) {
         int userId = i;
         int startOpId = i * opsPerThread;
+        // 最后一个线程处理剩余操作（避免整除问题）
         int actualOps = (i == threadNum - 1) ? kTotalOps - startOpId : opsPerThread;
 
-        iom.scheduler([&client, userId, startOpId, actualOps]() {
+        // 启动普通线程执行任务（替代协程任务）
+        threads.emplace_back([&client, userId, startOpId, actualOps]() {
             userTask(client, userId, startOpId, actualOps);
         });
     }
 
-    // 等待所有任务完成
-    iom.stop();
-    auto end = now();
-  
-  auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-  double duration = duration_ms / 1000.0;  // 转换为秒
+    // 5. 等待所有普通线程完成（替代协程iom.stop()）
+    for (auto& t : threads) {
+        t.join();
+    }
 
-    // 输出业务统计
+    // 6. 计算耗时与输出统计（不变）
+    auto end = now();
+    auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    double duration = duration_ms / 1000.0;  // 转换为秒
+
     std::cout << "\n=== 业务统计 ===" << "\n";
     std::cout << "总操作数: " << kTotalOps << "\n";
     std::cout << "总耗时: " << duration << "s\n";
